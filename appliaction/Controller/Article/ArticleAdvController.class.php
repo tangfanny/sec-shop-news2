6341277430e1d476a1559b22fa10d88cc405e62a
tangfanny <tangfanny@sina.cn>
2017-07-12 18:31:30 +0800

    pri

--- appliaction/Controller/Article/ArticleAdvController.class.php
+++ appliaction/Controller/Article/ArticleAdvController.class.php
@@ -18,7 +18,7 @@ class ArticleAdvController extends AdminBaseController {
 			if($pos_id){
 				$sqlmap['position_id'] = $pos_id;
 			}
-			$_order=isset($_POST['order']) ? ($_POST['order']) : NULL;
+			$_order = isset($_POST['order']) ? ($_POST['order']) : NULL;
 			$_sort = isset($_POST['sort']) ? ($_POST['sort']) : NULL;
 			if($_order && $_sort){
 				$order[$_sort] = $_order;
