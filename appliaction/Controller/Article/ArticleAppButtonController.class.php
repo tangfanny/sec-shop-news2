65cc5c9664b1c4cb306a60ae03e9bc4486b1287d
tangfanny <tangfanny@sina.cn>
2017-10-19 18:13:39 +0800

    pri

--- appliaction/Controller/Article/ArticleAppButtonController.class.php
+++ appliaction/Controller/Article/ArticleAppButtonController.class.php
@@ -39,6 +39,7 @@ class ArticleAppButtonController extends AdminBaseController{
             include $this->admin_tpl('article_app_button_lists');
         }
     }
+    
     /**
      * 添加修改页
      */
